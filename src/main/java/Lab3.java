6baf32b42648de2b22f7bcc25f16e2f4073040f8
rrout2 <rout.rishav@gmail.com>
2017-09-26 11:59:31 -0500

    lab3

--- src/main/java/Lab3.java
+++ src/main/java/Lab3.java
@@ -1,17 +1,35 @@
 import java.io.IOException;
 import java.net.URL;
 import java.util.Scanner;
-
-public class lab3 {
-
+/**
+ * lab3.
+ * @author rishav
+ *
+ */
+public class Lab3 {
+    /**
+     * main method.
+     * @param args var
+     */
     public static void main(final String[] args) {
+        Scanner kb = new Scanner(System.in);
         String url = urlToString("http://erdani.com/tdpl/hamlet.txt");
-        String[] splitted = url.split("\\s+");
+        String[] splitted = url.split("[ ,.!?;()-]");
         System.out.println(splitted.length);
-        System.out.println(countWord("prince", splitted));
+        System.out.println("What would you like to be counted?");
+        String findMe = kb.nextLine();
+        System.out.println("This word occurs " + countWord(findMe, splitted) + " times.");
+        kb.close();
 
 
     }
+
+
+    /**
+     * some.
+     * @param url need to be converted
+     * @return ural as a string
+     */
     public static String urlToString(final String url) {
         Scanner urlScanner;
         try {
@@ -23,6 +41,13 @@ public class lab3 {
         urlScanner.close();
         return contents;
     }
+
+    /**
+     * counts number of words.
+     * @param word to be counted
+     * @param list whole page
+     * @return num of instances
+     */
     public static int countWord(final String word, final String[] list) {
         int ct = 0;
         for (String s : list) {
